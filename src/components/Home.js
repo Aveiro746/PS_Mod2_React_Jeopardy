4a2cced115f8f3a730e404bcc943c7ec79599cac
Aveiro746 <95100461+Aveiro746@users.noreply.github.com>
2022-02-28 14:59:43 -0500

    Completed button link between components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const Home = (props) => {
+    console.log(props.jeopardy)
     return (
         <div>
             <h1>Welcome to Jeopardy! </h1>
-            <p>{props.jeopardy ? props.jeopardy.category.title: "nothing to show yet"}</p>
+            <p>{props.jeopardy ? props.jeopardy.category.title: "loading"}</p>
+            <button id= "startBtn"> <Link to = "/question">Start!</Link></button> 
+            
         </div>
     )
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Question = (props) => {
     console.log(props)
     return(
         <div>
             <h1>Question:</h1>
-            {props.jeopardy ? props.jeopardy.question: "no question" }
+            {props.jeopardy ? props.jeopardy.question: "Loading" }
+            <div>
+                <button id= "nextBtn"> <Link to = "/score">Answer!</Link></button> 
+            </div>
         </div>
     )
 }
